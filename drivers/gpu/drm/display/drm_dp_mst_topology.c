a2398c2a386b881f1ef5d16ef1c55452f6967f78
Fangzhi Zuo <Jerry.Zuo@amd.com>
2023-09-13 16:50:13 -0400

    drm: check connector state before dereference
    We are seeing the crash in the wild that we cannot repro ourselves.
    We want to be able to gather more data and the code should never be
    allowed to crash.
    
    Link: https://gitlab.freedesktop.org/drm/amd/-/issues/2314#note_2080187
    Suggested-by: John Lindgren <john@jlindgren.net>
    Signed-off-by: Fangzhi Zuo <Jerry.Zuo@amd.com>

--- drivers/gpu/drm/display/drm_dp_mst_topology.c
+++ drivers/gpu/drm/display/drm_dp_mst_topology.c
@@ -4289,7 +4289,9 @@ int drm_dp_atomic_find_time_slots(struct drm_atomic_state *state,
 		return PTR_ERR(topology_state);
 
 	conn_state = drm_atomic_get_new_connector_state(state, port->connector);
-	topology_state->pending_crtc_mask |= drm_crtc_mask(conn_state->crtc);
+	WARN_ON(!conn_state);
+	if (conn_state)
+		topology_state->pending_crtc_mask |= drm_crtc_mask(conn_state->crtc);
 
 	/* Find the current allocation for this port, if any */
 	payload = drm_atomic_get_mst_payload_state(topology_state, port);
@@ -4374,12 +4376,14 @@ int drm_dp_atomic_release_time_slots(struct drm_atomic_state *state,
 	bool update_payload = true;
 
 	old_conn_state = drm_atomic_get_old_connector_state(state, port->connector);
-	if (!old_conn_state->crtc)
+	WARN_ON(!old_conn_state);
+	if (!old_conn_state || !old_conn_state->crtc)
 		return 0;
 
 	/* If the CRTC isn't disabled by this state, don't release it's payload */
 	new_conn_state = drm_atomic_get_new_connector_state(state, port->connector);
-	if (new_conn_state->crtc) {
+	WARN_ON(!new_conn_state);
+	if (new_conn_state && new_conn_state->crtc) {
 		struct drm_crtc_state *crtc_state =
 			drm_atomic_get_new_crtc_state(state, new_conn_state->crtc);
 
@@ -4406,7 +4410,7 @@ int drm_dp_atomic_release_time_slots(struct drm_atomic_state *state,
 		return -EINVAL;
 	}
 
-	if (new_conn_state->crtc)
+	if (new_conn_state && new_conn_state->crtc)
 		return 0;
 
 	drm_dbg_atomic(mgr->dev, "[MST PORT:%p] TU %d -> 0\n", port, payload->time_slots);
